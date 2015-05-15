b63581793e05bacf96c9300ce1fdd8fcae01ddb5
Rocky Tang <rocky.tang168@gmail.com>
2015-05-14 18:58:54 -0700

    adds CORS headers

--- db_keys.js
+++ db_keys.js
@@ -1,4 +1,5 @@
 var db_url = "postgres://ypjevzmsxuzpjg:w1x5ZaFg-ppJJAuzTEktul8esy@ec2-54-243-187-196.compute-1.amazonaws.com:5432/depnp1l42hj5tf";
 
+//ec2-107-20-152-139.compute-1.amazonaws.com
 
 module.exports = db_url;
